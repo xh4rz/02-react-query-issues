7c626e1af5f53c0ca32b0d437177c67baadd4462
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-04-11 22:39:34 -0500

    Instalación y explicación del proyecto

---
+++ src/issues/components/LabelPicker.tsx
@@ -0,0 +1,12 @@
+export const LabelPicker = () => {
+  return (
+    <>
+      <span
+        className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      >
+        Primary
+      </span>
+    </>
+  );
+};
