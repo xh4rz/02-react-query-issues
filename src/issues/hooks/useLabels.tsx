38e9e508e9b95d4e7c849cdbfb6a572462500c6b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-04-16 01:09:09 -0500

    initialData y placeholderData

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -1,11 +1,50 @@
 import { useQuery } from '@tanstack/react-query';
 import { getLabels } from '../actions';
+import { GithubLabel } from '../interfaces';
 
 export const useLabels = () => {
 	const labelsQuery = useQuery({
 		queryKey: ['labels'],
 		queryFn: getLabels,
-		staleTime: 1000 * 60 * 60 // 1 hora de stale time
+		staleTime: 1000 * 60 * 60, // 1 hora de stale time
+		// placeholderData: [
+		// 	{
+		// 		id: 69105383,
+		// 		node_id: 'MDU6TGFiZWw2OTEwNTM4Mw==',
+		// 		url: 'https://api.github.com/repos/facebook/react/labels/Browser:%20IE',
+		// 		name: 'Browser: IE',
+		// 		color: 'c7def8',
+		// 		default: false
+		// 	} satisfies GithubLabel,
+
+		// 	{
+		// 		id: 739777675,
+		// 		node_id: 'MDU6TGFiZWw3Mzk3Nzc2NzU=',
+		// 		url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Component%20API',
+		// 		name: 'Component: Component API',
+		// 		color: 'd4c5f9',
+		// 		default: false
+		// 	} satisfies GithubLabel
+		// ]
+		initialData: [
+			{
+				id: 69105383,
+				node_id: 'MDU6TGFiZWw2OTEwNTM4Mw==',
+				url: 'https://api.github.com/repos/facebook/react/labels/Browser:%20IE',
+				name: 'Browser: IE',
+				color: 'c7def8',
+				default: false
+			} satisfies GithubLabel,
+
+			{
+				id: 739777675,
+				node_id: 'MDU6TGFiZWw3Mzk3Nzc2NzU=',
+				url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Component%20API',
+				name: 'Component: Component API',
+				color: 'd4c5f9',
+				default: false
+			} satisfies GithubLabel
+		]
 	});
 
 	return {
