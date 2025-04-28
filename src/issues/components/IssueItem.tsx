3628f6ca584ffaa13f99dd9eef2f85e61df0e6d6
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-04-28 01:18:50 -0500

    Actualizar la fecha de creación

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { GithubIssue, State } from '../interfaces';
 import { useQueryClient } from '@tanstack/react-query';
 import { getIssue, getIssueComments } from '../actions';
+import { timeSince } from '../../helpers';
 
 interface Props {
 	issue: GithubIssue;
@@ -52,7 +53,7 @@ export const IssueItem = ({ issue }: Props) => {
 					{issue.title}
 				</a>
 				<span className="text-gray-500">
-					#{issue.number} opened 2 days ago by{' '}
+					#{issue.number} opened {timeSince(issue.created_at)} ago by{' '}
 					<span className="font-bold">{issue.user.login}</span>
 				</span>
 
